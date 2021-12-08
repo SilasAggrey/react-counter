b1d61430e0f564e9a495c826626f07513ccaedda
SilasAggrey <91203655+SilasAggrey@users.noreply.github.com>
2021-12-08 02:50:08 -1200

    react-counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+//  class App extends React.Component {
+//    constructor() {
+//      super();
+
+//      this.state = {
+//        number: 2,
+//      };
+//        }
+
+//    handleIncrease = () => {
+//      this.setState({ number: this.state.number + 1,  });
+//     };
+//     handleDecrease = () => {
+//       this.setState({ number: this.state.number -1, 
+
+//       })
+//     };
+
+
+//    render() {
+//      return (
+//        <div className="App">
+//          <h1>{this.state.number}</h1>
+//          <h3>{this.state.isHungry}</h3>
+//          <button onClick={this.handleIncrease}>Add 1</button>
+//          <button onClick={this.handleDecrease}>Remove 1</button>
+//        </div>
+//      );
+//    }
+//  }
+
+ function App() {
+      const [number, setNumber] = React.useState(10);
+
+   const addOne = () => {
+     setNumber(number + 1);
+    
+   };
+   const removeOne = () => {
+      setNumber(number - 1);
+   };
+
+   return (
+     <div className="App">
+       <h1>{number}</h1>
+       <button onClick={addOne}>Add one</button>
+       <button onClick={removeOne}>Remove one</button>
+     </div>
+   );
+ }
+
+
+
 
 export default App;
